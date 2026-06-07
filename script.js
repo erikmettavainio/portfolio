7f12a7b8c94695066eb9704e32e3dc38fbf8a235
erikmettavainio <erikmett2@gmail.com>
2026-06-07 14:39:50 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+const cursor =
+document.querySelector(".cursor");
+
+let mouseX = 0;
+let mouseY = 0;
+
+let currentX = 0;
+let currentY = 0;
+
+document.addEventListener("mousemove",(e)=>{
+
+mouseX = e.clientX;
+mouseY = e.clientY;
+
+});
+
+function animate(){
+
+currentX += (mouseX-currentX)*0.08;
+currentY += (mouseY-currentY)*0.08;
+
+cursor.style.left =
+currentX + "px";
+
+cursor.style.top =
+currentY + "px";
+
+requestAnimationFrame(animate);
+}
+
+animate();
